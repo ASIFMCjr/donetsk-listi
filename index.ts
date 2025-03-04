6efd2a3befd730f489ffca5989b0d9802a06ce87
Bogdan Balandin <b.balandin@wis.software>
2025-03-04 13:13:06 +0300

    updated show products logic

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 const fetchHandler = async (request: Request): Promise<Response> => {
   const url = new URL(request.url);
   const indexRoutes = ['/','/catalog','/about','/contacts'];
+  const catalogRegex = /\/catalog\/product-(1[0-1]|[1-9])\b/g;
   if (url.pathname === "/catalog.pdf") return new Response(Bun.file("catalog.pdf"));
   if (url.pathname.includes("/assets/")) return new Response(Bun.file("." + url.pathname))
-  if (indexRoutes.includes(url.pathname)) return new Response(Bun.file("index.html"));
+  if (indexRoutes.includes(url.pathname) || catalogRegex.test(url.pathname)) return new Response(Bun.file("index.html"));
   return new Response("404!");
 }
 
